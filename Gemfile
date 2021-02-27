4f4b0896c70c97b004eed3b6c1362550757abee6
Aleksander Buhara <alexander.bugara@gmail.com>
2021-02-27 17:11:36 +0200

    updated regarding code review

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@
 source "https://rubygems.org"
 
 gem 'rubocop', '~>1.10.0'
-
+gem 'benchmark', '~>0.1.1'
 
 #git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 # gem "rails"
--- validate.rb
+++ validate.rb
@@ -1,38 +1,24 @@
 # frozen_string_literal: true
 
 NINE = 9
-
 TWO = 2
-
 TEN = 10
+CC_LENGTH = 16
+CC_LENGTH_WITH_SEPARATORS = 19
+CC_INPUT_ERROR_MESSAGE = 'Your cc input has an error'
 
-CVV_LENGTH = 16
-
-CVV_LENGTH_WITH_SEPARATORS = 19
-
-SEPARATOR_POSITION = 5
-
-COUNTER_INCREMENT = 1
-
-EXCLUDE_INDEX1 = 4
-
-EXCLUDE_INDEX2 = 9
-
-EXCLUDE_INDEX3 = 14
-
-CVV_INPUT_ERROR_MESSAGE = 'Your cvv input has an error'
-
-def validate(cvv)
-  validate_input(cvv)
-  normilised_cvv = normilise_if_need_it(cvv)
-  check_digit = get_check_sum(normilised_cvv)
+def validate(ccn)
+  validate_input(ccn)
+  ccn = normalise(ccn) if ccn.match?("[0-9]{4}[\-:\s][0-9]{4}[\-:\s][0-9]{4}[\-:\s][0-9]{4}")
+  check_digit = get_check_sum(ccn)
   (check_digit % TEN).zero?
 rescue ArgumentError
-  puts CVV_INPUT_ERROR_MESSAGE
+  puts CC_INPUT_ERROR_MESSAGE
+  false
 end
 
-def get_check_sum(cvv)
-  cvv.chars.each_with_index.reduce(0) do |memo, (element, index)|
+def get_check_sum(ccn)
+  ccn.chars.each_with_index.reduce(0) do |memo, (element, index)|
     temp_digit = element.to_i
     unless (index + 1).even?
       temp_digit *= TWO
@@ -42,21 +28,28 @@ def get_check_sum(cvv)
   end
 end
 
-def validate_input(cvv)
-  raise ArgumentError if cvv.empty?
-  raise ArgumentError if cvv.size != CVV_LENGTH && cvv.size != CVV_LENGTH_WITH_SEPARATORS
+def validate_input(ccn)
+  raise ArgumentError if ccn.empty?
+  raise ArgumentError if ccn.size != CC_LENGTH && ccn.size != CC_LENGTH_WITH_SEPARATORS
 end
 
-def normilise_if_need_it(cvv)
-  if cvv.size == CVV_LENGTH_WITH_SEPARATORS
-    collection = cvv.chars.clone
-    indexes_to_reject = [EXCLUDE_INDEX1, EXCLUDE_INDEX2, EXCLUDE_INDEX3]
-    result = collection.reject.each_with_index { |_, ix| indexes_to_reject.include? ix }
-    return result.join if result.join.match?(/\A-?\d+\Z/)
-
-    raise ArgumentError
-  end
-  cvv
+def normalise(ccn)
+  ccn.gsub(/[\- \s]/, '-' => '', ' ' => '')
 end
 
+p '------------success cases-----------------'
+p '4561 2612 1234 5467'
+p validate('4561 2612 1234 5467')
+p '>>'
+p '5425-2334-3010-9903'
 p validate('5425-2334-3010-9903')
+
+p '------------fail cases-----------------'
+p '45-61 2612 1234 546-7'
+p validate('45-61 2612 1234 546-7')
+p '>>'
+p '4561 2612 1234 5469'
+p validate('4561 2612 1234 5469')
+p '>>'
+p '\' \''
+p validate('')
