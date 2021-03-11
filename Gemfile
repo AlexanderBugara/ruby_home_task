05302b49299b45058adbd66359e712485ebc7ecd
oleksandr.buhara <oleksandr.buhara@headspace.com>
2021-03-11 23:38:01 +0200

    add logger

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source "https://rubygems.org"
 
 gem 'rubocop', '~>1.10.0'
 gem 'benchmark', '~>0.1.1'
-
+gem 'logger'
+gem 'tmpdir'
 #git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 # gem "rails"
--- validate.rb
+++ validate.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require 'logger'
+require 'tmpdir'
+
 NINE = 9
 TWO = 2
 TEN = 10
@@ -9,8 +12,49 @@ CC_INPUT_ERROR_MESSAGE = 'Your cc input has an error'
 MINIMUM_AGE = 13
 AGE_VIOLATION_MESSAGE = 'Your age is under 13 years old. You can\'t do this operation'
 
+# generic interface for User and UserWrapper
+module UserAbstract
+  def initialize(name, age, card) end
+  attr_reader :name, :age, :card
+end
+
+# To make logs
+class UserWrapper
+  include UserAbstract
+  attr_reader :user, :logger
+
+  def initialize(name, age, card)
+    @user = User.new(name, age, card)
+    tmp_logger = Logger.new File.open('/tmp/ruby_user.log', 'a')
+    tmp_logger.level = Logger::INFO
+    @logger = tmp_logger
+  end
+
+  def validate_credit_card
+    logger.info(">> validate ccn #{user.card.ccn}")
+    logger.info(">> validation is #{user.validate_credit_card}")
+  end
+
+  def name
+    logger.info(">> accessing user name #{user.name}")
+    user.name
+  end
+
+  def age
+    logger.info(">> accessing user age #{user.age}")
+    user.age
+  end
+
+  def card
+    logger.info(">> accessing card #{user.card.ccn}")
+    user.card
+  end
+end
+
 # User with name, age and card
 class User
+  include UserAbstract
+
   attr_accessor :name, :age, :card
 
   def initialize(name, age, card)
@@ -110,3 +154,17 @@ card = Card.new('4561 26 12 123 4 5469')
 user = User.new('Alex', 101, card)
 
 p user.validate_credit_card
+
+p '------------with logger-----------------'
+
+card = Card.new('4561 2612 1234 5467')
+wrapper = UserWrapper.new('Alex', 13, card)
+wrapper.name
+wrapper.age
+wrapper.validate_credit_card
+
+card = Card.new('5425-2334-3010-9903')
+wrapper = UserWrapper.new('Goseph', 12, card)
+wrapper.name
+wrapper.age
+wrapper.validate_credit_card
